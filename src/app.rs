bded982d8d5232cb514acfd27855b56d2b543fb5
Charlie <ce@yagro.com>
2025-01-12 12:53:04 +0000

    Hook up SQLX connection and table widget

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,23 @@
+use egui::TextWrapMode;
+use egui_extras::{Column, TableBuilder};
+use sqlx::{Pool, Postgres};
+use tokio::runtime::Runtime;
+
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct Rosemary {
     code: String,
+    #[serde(skip)]
+    db_pool: Option<Pool<Postgres>>,
+    query_result: String,
 }
 
 impl Default for Rosemary {
     fn default() -> Self {
         Self {
             code: "".to_owned(),
+            db_pool: None,
+            query_result: "No results yet.".to_owned(),
         }
     }
 }
@@ -15,10 +25,23 @@ impl Default for Rosemary {
 impl Rosemary {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+            let mut app: Rosemary = eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+            app.initialise_db();
+            return app;
         }
 
-        Default::default()
+        let mut app = Rosemary::default();
+        app.initialise_db();
+        app
+    }
+
+    fn initialise_db(&mut self) {
+        let runtime = Runtime::new().expect("Failed to create runtime");
+        self.db_pool = Some(runtime.block_on(async {
+            sqlx::PgPool::connect("")
+                .await
+                .expect("Failed to connect to db")
+        }))
     }
 }
 
@@ -66,6 +89,78 @@ impl eframe::App for Rosemary {
                     .desired_width(f32::INFINITY)
                     .layouter(&mut layouter),
             );
+
+            if ui.add(egui::Button::new("Execute")).clicked() {
+                if let Some(query_str) = self.code.clone().into() {
+                    let db_pool = self.db_pool.clone();
+                    let result_ref = &mut self.query_result;
+                    let runtime = Runtime::new().expect("Failed to create runtime");
+
+                    runtime.block_on(async move {
+                        if let Some(pool) = db_pool {
+                            match sqlx::query(&query_str).fetch_all(&pool).await {
+                                Ok(rows) => {
+                                    *result_ref = String::from(format!("Results: {:?}", rows));
+                                    println!("{:?}", &result_ref);
+                                }
+                                Err(_) => {
+                                    println!("Failed");
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+
+            ui.separator();
+
+            let columns = [
+                String::from("example"),
+                String::from("example2"),
+                String::from("example3"),
+            ];
+
+            let available_height = ui.available_height();
+            let mut table = TableBuilder::new(ui)
+                .striped(true)
+                .resizable(true)
+                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                .min_scrolled_height(0.0)
+                .max_scroll_height(available_height);
+
+            for _ in &columns {
+                table = table.column(Column::auto());
+            }
+
+            let text_height = 40 as f32;
+
+            table
+                .header(20.0, |mut header| {
+                    for column_name in &columns {
+                        header.col(|ui| {
+                            ui.strong(column_name);
+                        });
+                    }
+                })
+                .body(|body| {
+                    body.rows(text_height, 100, |mut row| {
+                        let row_index = row.index() + 1;
+
+                        row.set_selected(false);
+
+                        row.col(|ui| {
+                            ui.label(row_index.to_string());
+                        });
+                        row.col(|ui| {
+                            ui.add(
+                                egui::Label::new("Thousands of rows of even height")
+                                    .wrap_mode(TextWrapMode::Extend),
+                            );
+                        });
+                        //TODO: do the same as the table example for the rows here over the pgvec
+                        //result
+                    })
+                });
         });
     }
 }
